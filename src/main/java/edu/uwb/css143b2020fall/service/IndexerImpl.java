4ae89eae525ad425a2e577b92b39958de498c32d
Martin Matsudaira <mmats23@uw.edu>
2020-12-06 19:19:02 -0800

    added some context to indexer and searcher

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -2,16 +2,30 @@ package edu.uwb.css143b2020fall.service;
 
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class IndexerImpl implements Indexer {
     public Map<String, List<List<Integer>>> index(List<String> docs) {
         Map<String, List<List<Integer>>> indexes = new HashMap<>();
         // add your code
+
+        //split doc name into strings for all docs
+        List<List<String[]>> docNameSplit = new ArrayList<>();
+        List<String[]> keyWords = new ArrayList<>();
+
+        for(String name : docs) {
+            keyWords.clear();
+            keyWords.add(name.split("\\s+"));
+            docNameSplit.add(keyWords);
+        }
+
+        //find keyword for all docs
+
+        //find all location in each doc
+
+        //add to hashmap
+
         return indexes;
     }
 }
--- src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
@@ -16,6 +16,18 @@ public class SearcherImpl implements Searcher {
     public List<Integer> search(String keyPhrase, Map<String, List<List<Integer>>> index) {
         List<Integer> result = new ArrayList<>();
         // add your code
+
+        //split keyphrase by spaces
+        String[] keyWords = keyPhrase.split("\\s+");
+
+        //get all docs that contain all words of the given phrase
+
+
+        //get the intersection of lists for the list of docs
+
+        //find if keyphrase found is in the right place and order for the list of words in docs
+
+
         return result;
     }
 }
